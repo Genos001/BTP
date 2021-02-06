cf886f8ca61e1cdd96b8576e40a0cdf01a5f91ed
Genos001 <viishalyadav250229@gmail.com>
2021-02-06 18:37:54 +0530

    2

---
+++ color_det.py
@@ -0,0 +1,198 @@
+import cv2
+import numpy as np
+import pandas as pd
+import time
+import RPi.GPIO as GPIO
+import pyttsx3
+# from pygame import mixer
+
+
+
+
+btn_pin = 15
+mode = 1 # 1 for color and 0 for ph
+st_time = -1
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+prev_result = "nill"
+count = 0
+
+engine = pyttsx3.init('espeak')
+engine.setProperty('rate', 160)
+engine.setProperty('volume',1.0)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(btn_pin, GPIO.IN)
+# mixer.init()
+engine.say('Hello Boys') 
+engine.runAndWait()
+
+def func_mode2():
+
+    hsv_frame = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+     # Red color
+    low_red = np.array([0, 60, 105])
+    high_red = np.array([21, 187, 175])
+    red_mask = cv2.inRange(hsv_frame, low_red, high_red)
+    Red = cv2.countNonZero(red_mask)
+
+    # Blue color
+    low_blue = np.array([70, 58, 15])
+    high_blue = np.array([125, 235, 190])
+    blue_mask = cv2.inRange(hsv_frame, low_blue, high_blue)
+    Blue = cv2.countNonZero(blue_mask)
+
+    # ph color
+    low_green = np.array([16, 40, 86])
+    high_green = np.array([35, 180, 205])
+    green_mask = cv2.inRange(hsv_frame, low_green, high_green)
+    Ph = cv2.countNonZero(green_mask)
+
+    if(Red > Blue and Red > 50):
+        print("red")
+        engine.say('red') 
+        engine.runAndWait() 
+
+    elif(Blue > Red and Blue> 50 ):
+        print("blue")
+        engine.say('blue') 
+        engine.runAndWait() 
+
+    elif(Ph > 2000 ):
+        print("ph paper ",Ph )
+        engine.say('paper detected') 
+        engine.runAndWait() 
+
+    else:
+        print("no")
+        engine.say('no paper detected') 
+        engine.runAndWait() 
+
+
+
+def func_mode1():
+
+    
+    roi = img[150:350, 200:400]
+    #cv2.imshow('CAMERAa',roi)
+
+    hsv_frame = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+
+     # Red color
+    low_pink = np.array([138, 50, 60])
+    high_pink = np.array([166, 255, 186])
+    pink_mask = cv2.inRange(hsv_frame, low_pink, high_pink)
+    pink = cv2.countNonZero(pink_mask)
+
+    low_vio = np.array([94, 21, 9])
+    high_vio = np.array([165, 134, 217])
+    vio_mask = cv2.inRange(hsv_frame, low_vio, high_vio)
+    vio = cv2.countNonZero(vio_mask)
+
+
+    low_yell = np.array([25, 65, 95])
+    high_yell = np.array([47, 125, 195])
+    yell_mask = cv2.inRange(hsv_frame, low_yell, high_yell)
+    yell = cv2.countNonZero(yell_mask)
+
+    low_green = np.array([30, 163, 46])
+    high_green = np.array([67, 227,179])
+    green_mask = cv2.inRange(hsv_frame, low_green, high_green)
+    green = cv2.countNonZero(green_mask)
+
+
+    low_blue = np.array([91, 185, 121])
+    high_blue = np.array([129, 255, 201])
+    blue_mask = cv2.inRange(hsv_frame, low_blue, high_blue)
+    blue = cv2.countNonZero(blue_mask)
+
+
+    low_red = np.array([155, 150, 155])
+    high_red = np.array([180, 255, 250])
+    red_mask = cv2.inRange(hsv_frame, low_red, high_red)
+    red = cv2.countNonZero(red_mask)
+
+    new = max(pink, green, yell, red, blue, vio)
+    print (new)
+    if new==pink:
+        print("pink")
+        new = "pink"
+    elif new==green:
+        print("green")
+        new = "green"
+    elif new==yell:
+        print("yellow")
+        new = "yellow"
+    elif new==red:
+        print("red")
+        new = "red"
+    elif new==blue:
+        print("blue")
+        new = "blue"
+    else:
+        print("vio")
+        new = "violet"
+    #print (new)
+    engine.say(new) 
+    engine.runAndWait() 
+
+
+while True:
+    success, img = cap.read()
+    cv2.rectangle(img, (200,150), (400,350), (0,255,0), 1) 
+    time.sleep(0.1)
+    if (GPIO.input(btn_pin) == False):
+        time.sleep(0.01)
+        if (GPIO.input(btn_pin) == False):
+            time.sleep(0.3)
+            if (GPIO.input(btn_pin) == True):
+                count+=1
+                if(st_time==-1):
+                    st_time = time.time()
+
+    if(st_time!=-1):
+        if(time.time()-st_time>1 and time.time()-st_time<1.5):
+            print(count)
+            if(count==1):
+                print(mode)
+                if(mode):
+                    print("m1")
+                    func_mode1()
+                else:
+                    print("m2")
+                    func_mode2()
+
+            else:
+                mode = not mode
+                if(mode):
+                    print("changed mode 1")
+                    engine.say('color detection mode') 
+                    engine.runAndWait() 
+
+                else:
+                    print("changed mode 2")
+                    engine.say('paper detection mode') 
+                    engine.runAndWait() 
+            count=0
+            st_time=-1;
+
+        if(time.time()-st_time>1.5):
+            count=0
+            st_time=-1
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+
+    #img = cv2.flip(img,1)
+    
+    cv2.imshow('CAMERA',img)
+        
+cap.release()
+
+cv2.destroyAllWindows()
+
---
+++ number_det.py
@@ -0,0 +1,186 @@
+from imutils.perspective import four_point_transform
+from imutils import contours
+import imutils
+import cv2
+import numpy as np
+import time
+import RPi.GPIO as GPIO
+#from PIL import Image
+# from pygame import mixer
+import shlex
+from subprocess import Popen, PIPE
+import re
+import pyttsx3
+
+
+
+btn_pin = 15
+st_time = -1
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+
+engine = pyttsx3.init('espeak')
+engine.setProperty('rate', 130)
+engine.setProperty('volume',1.0)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(btn_pin, GPIO.IN)
+# mixer.init()
+
+
+
+def bird_view(image):
+    image = imutils.resize(image, height=500)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(blurred, 220, 92, 255)
+
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    displayCnt = None
+
+    # loop over the contours
+    val=0 
+    max_val=-1
+    warped=0
+    output=0
+    err = 0
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # if the contour has four vertices, then we have found
+        # the display
+        if len(approx) == 4:
+            displayCnt = approx
+            warped_0 = four_point_transform(gray, displayCnt.reshape(4, 2))
+            output_0 = four_point_transform(image, displayCnt.reshape(4, 2))
+
+            hsv_frame = cv2.cvtColor(output_0, cv2.COLOR_BGR2HSV)
+            low_green = np.array([70, 229, 188])
+            high_green = np.array([152, 255, 255])
+            green_mask = cv2.inRange(hsv_frame, low_green, high_green)
+            val = cv2.countNonZero(green_mask)
+
+            if(val>max_val):
+                max_val = val
+                warped = warped_0
+                output = output_0
+                err = 1
+
+
+            # break
+    # warped = four_point_transform(gray, displayCnt.reshape(4, 2))
+    # output = four_point_transform(image, displayCnt.reshape(4, 2))
+    return warped, output, err
+
+while True:
+    success, image = cap.read()
+    time.sleep(0.1)
+    if (GPIO.input(btn_pin) == False):
+        time.sleep(0.02)
+        if (GPIO.input(btn_pin) == False):
+            time.sleep(0.4)
+            if (GPIO.input(btn_pin) == True):
+
+                warped, output, err = bird_view(image)
+                if(err==0):
+                    print("screen not found try again")
+                    continue
+                
+                height, width, channels = output.shape
+                x0=int(width/6)+3
+                y0=5
+                x1=width-8
+                y1=height-10
+                output = output[y0:y1, x0:x1]
+                try:
+                    output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+                except:
+                    print("error")
+                #cv2.rectangle(output, (x0,y0), (x1,y1), (0,255,0), 1)
+                
+                # output = cv2.adaptiveThreshold(warped, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                #         cv2.THRESH_BINARY, 11, 2)
+                try:
+                    print("applying adp")
+                    output = cv2.adaptiveThreshold(output, 255, cv2.ADAPTIVE_THRESH_MEAN_C, 
+                                                      cv2.THRESH_BINARY, 11, 2)
+                except:
+                    print("adaptive threshold error")
+
+                #cv2.imshow('output',output)
+                kernel = np.ones((4,4),np.uint8)
+                output = cv2.morphologyEx(output, cv2.MORPH_CLOSE, kernel)
+                kernel = np.ones((2,2),np.uint8)
+                output = cv2.erode(output,kernel,iterations = 1)
+                cv2.imwrite(r'/home/pi/a.jpg', output)
+                #cv2.imshow('output2',output)
+            
+                cmd = "ssocr --number-digits=-1 --charset=digits -T a.jpg"
+                process = Popen(shlex.split(cmd), stdout=PIPE)
+                (out, err)=process.communicate()
+                print(err)
+                #digits
+                out=out.decode("utf-8") 
+                temp = ",".join(re.findall(r'\d+', out))
+                
+                length = len(temp)
+                if(length < 3):
+                    print("seg fault try again")
+                    continue
+                indices = [0,length-2,length-1,length]
+                parts = [temp[i:j] for i,j in zip(indices, indices[1:]+[None])]
+                
+                print(temp)
+                print(parts)
+                engine.say(parts[0]) 
+                engine.runAndWait()
+                
+                engine.setProperty('rate', 250)
+                engine.say('point') 
+                engine.runAndWait()
+                
+                engine.say(parts[1]) 
+                engine.runAndWait()
+                
+                engine.say(parts[2]) 
+                engine.runAndWait()
+                
+                engine.say('gram') 
+                engine.runAndWait()
+                #exit_code = process.wait()
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    #img = cv2.flip(image,1)
+    
+    #cv2.imshow('CAMERA',image)
+        
+cap.release()
+
+cv2.destroyAllWindows()
+
+
+
+# image = cv2.imread('../Images_to_test/z ('+str(i)+').jpeg')
+
+# bigger = cv2.resize(image, (400, 600)) 
+# # cv2.imshow('output',bigger)
+
+
+
+# # kernel = np.ones((2,2),np.uint8)
+# # output = cv2.erode(output,kernel,iterations = 1)
+# #output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+# cv2.imshow('out'+str(i),output)
+# #cv2.imwrite(r'C:\Users\viish\Desktop\nat_out\NRE4'+ str(i) +'.jpg', output)
+
+# cv2.waitKey()
