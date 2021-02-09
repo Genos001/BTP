ef5d6b8f0006ea92a1a687cd74d1a8dc9ce2cc2c
Genos001 <Vishal@250>
2021-02-10 02:06:59 +0530

    7

--- music/call.py
+++ music/call.py
@@ -1,13 +1,19 @@
 import requests
 
 
-for i in range(0,221):
-	for j in range(0,10):
-		for k in range (0,10):
-			url = 'https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=' +str(i)+'.'+str(j)+str(k)+ '&tl=en&total=1&idx=0&textlen=9'
-			r = requests.get(url, allow_redirects=True)
+# for i in range(0,221):
+# 	for j in range(0,10):
+# 		for k in range (0,10):
+# 			url = 'https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=' +str(i)+'.'+str(j)+str(k)+ '&tl=en&total=1&idx=0&textlen=9'
+# 			r = requests.get(url, allow_redirects=True)
 
-			open( str(i)+'.'+str(j)+str(k)+'.mp3', 'wb').write(r.content)
-	print(i)
+# 			open( str(i)+'.'+str(j)+str(k)+'.mp3', 'wb').write(r.content)
+# 	print(i)
+
+for i in range(0,19):
+	url = 'https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=' +str(i)+ '&tl=en&total=1&idx=0&textlen=9'
+	r = requests.get(url, allow_redirects=True)
+
+	open( str(i)+'.mp3', 'wb').write(r.content)
 
 print ("end")
